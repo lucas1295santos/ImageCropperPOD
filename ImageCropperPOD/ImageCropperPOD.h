2660aa99b95552574fb89901ec839b39bf08ff97
Lucas Santos <lucas1295santos@gmail.com>
2018-04-12 13:56:55 -0300

    Initial Commit

---
+++ ImageCropperPOD/ImageCropperPOD.h
@@ -0,0 +1,19 @@
+//
+//  ImageCropperPOD.h
+//  ImageCropperPOD
+//
+//  Created by Lucas Santos on 12/04/18.
+//  Copyright © 2018 Lucas Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCropperPOD.
+FOUNDATION_EXPORT double ImageCropperPODVersionNumber;
+
+//! Project version string for ImageCropperPOD.
+FOUNDATION_EXPORT const unsigned char ImageCropperPODVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCropperPOD/PublicHeader.h>
+
+
