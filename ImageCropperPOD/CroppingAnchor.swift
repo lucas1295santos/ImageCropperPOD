0099b89e5c8e32999df119bd13745716626e02fe
Lucas Santos <lucas1295santos@gmail.com>
2018-04-17 16:37:22 -0300

    Adicionando comentarios e parametrizando

---
+++ ImageCropperPOD/CroppingAnchor.swift
@@ -0,0 +1,17 @@
+//
+//  CroppingAnchor.swift
+//  ImageCropperPOD
+//
+//  Created by Lucas Santos on 17/04/18.
+//  Copyright © 2018 Lucas Santos. All rights reserved.
+//
+
+import Foundation
+
+public enum CroppingAnchor {
+    case topLeft
+    case topRight
+    case bottonLeft
+    case bottonRight
+    case center
+}
